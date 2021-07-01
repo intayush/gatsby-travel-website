ae8bf082d8e9a12c3517fb8983ff178b36afd318
Ayush Singh <ayush.singh@newgen.co.in>
2021-07-01 18:45:41 +0530

    light image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import Button from "./generic/Button"
-import video from "../assets/videos/spiti.mp4"
+import video from "../assets/videos/video.mp4"
 
 const Hero = () => {
   return (
